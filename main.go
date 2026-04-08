49a97923b906e1d205c74449bdcbdda040da9d37
hammerheaddf <hammerhead.df@gmail.com>
2026-04-07 23:51:52 -0300

    Revert "Naming for full album files improved"
    This reverts commit d97111335ca2817ac9a6f4e28b806963592c5492.

--- main.go
+++ main.go
@@ -40,16 +40,6 @@ func downloadProperAlbum(albumURL string, rawBytes []byte, info PageInfo, downlo
 	imagesFound := crawlAlbumImages(bytes.NewReader(rawBytes))
 	albumDate, dateErr := getAlbumDate(bytes.NewReader(rawBytes))
 
-	// Extract album ID from URL for file naming (e.g. ".../album/5996040/...")
-	albumID := ""
-	urlParts := strings.Split(strings.TrimSuffix(albumURL, "/"), "/")
-	for i, p := range urlParts {
-		if p == "album" && i+1 < len(urlParts) {
-			albumID = urlParts[i+1]
-			break
-		}
-	}
-
 	fmt.Printf("Found %q set from %s — %d image(s). Downloading...\n", info.AlbumName, info.ModelName, len(imagesFound))
 
 	albumDir := downloadsDir + "/photos/" + info.ModelName + " - " + info.AlbumName
@@ -64,7 +54,7 @@ func downloadProperAlbum(albumURL string, rawBytes []byte, info PageInfo, downlo
 		wg.Add(1)
 		go func(i int, imageURL string) {
 			defer wg.Done()
-			imageOutput := fmt.Sprintf("%s/%s - %04d.jpg", albumDir, albumID, i+1)
+			imageOutput := albumDir + "/" + fmt.Sprintf("%04d", i+1) + ".jpg"
 			b, _ := saveImage(imageURL, imageOutput)
 			imagesDownloaded[i] = imageOutput
 			mu.Lock()
